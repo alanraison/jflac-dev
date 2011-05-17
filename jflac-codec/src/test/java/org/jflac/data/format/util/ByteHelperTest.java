20f822e0eec4bbb399d04a74ee2de73c6ea59e3a
Alan Raison <alanraison@users.sourceforge.net>
2011-05-17 09:57:08 +0100

    Finished ByteHelperTest

--- jflac-codec/src/test/java/org/jflac/data/format/util/ByteHelperTest.java
+++ jflac-codec/src/test/java/org/jflac/data/format/util/ByteHelperTest.java
@@ -22,19 +22,19 @@ import org.junit.Test;
  *
  */
 public class ByteHelperTest {
+	/**
+	 * Test the conversion of a byte array into an integer
+	 */
 	@Test
 	public void testMakeInt() {
 		for (int mul = 0; mul < 4; mul++) {
 			final int index = 3 - mul;
 			final int shift = (int) Math.pow(2, mul * 8);
 			for (int i = 1; i < 255; i++) {
-				final byte[] source = new byte[4]; //{ (byte) 0, (byte) 0, (byte) 0, (byte) 0 };
+				final byte[] source = new byte[4];
 				source[index] = (byte) i;
 				final int expected = i * shift;
-				System.out.printf("i=%d, expected=%d, mul=%d, shift=%d%n", i, expected, mul, shift);
 				Assert.assertEquals(expected, ByteHelper.makeInt(source));
-				//if (i % 100 == 0) {
-				//}
 			}
 		}
 	}
